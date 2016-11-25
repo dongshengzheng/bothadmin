31dcca72025f5ec60eaa52791e1f0f499c99ae0b
dongsheng.zheng@windyeel.com <ease4East>
2016-11-25 18:56:50 +0800

    tiny stuff fixed

---
+++ service/src/main/java/com/fish/idle/service/config/Global.java
@@ -0,0 +1,20 @@
+/**
+ * Copyright &copy; 2015-2020 <a href="http://www.xianyutec.org/">JeePlus</a> All rights reserved.
+ */
+package com.fish.idle.service.config;
+
+/**
+ * 全局配置类
+ *
+ * @author Han.Sun
+ * @version 2014-06-25
+ */
+public class Global {
+    /**
+     * 作品状态
+     */
+    public static final String WORKS_STATUS_DRAFT ="0";    // 草稿
+    public static final String WORKS_STATUS_COMMIT ="1";    // 审核中
+    public static final String WORKS_STATUS_UNPASS ="2";    // 未通过
+    public static final String WORKS_STATUS_PASS ="3";      // 已通过
+}
