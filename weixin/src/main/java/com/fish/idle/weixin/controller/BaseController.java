8667f8065fdf655f1c2897ba03bc9167df322878
dongsheng.zheng@windyeel.com <ease4East>
2016-12-13 14:12:31 +0800

    weixin, baseController added

---
+++ weixin/src/main/java/com/fish/idle/weixin/controller/BaseController.java
@@ -0,0 +1,61 @@
+package com.fish.idle.weixin.controller;
+
+import com.baomidou.mybatisplus.mapper.EntityWrapper;
+import com.baomidou.mybatisplus.plugins.Page;
+import com.fish.idle.service.util.Const;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.propertyeditors.CustomDateEditor;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
+
+import javax.servlet.http.HttpServletRequest;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+/**
+ * Created by szy on 03/12/2016.
+ */
+public class BaseController {
+
+    @Autowired
+    protected HttpServletRequest request;
+
+    /**
+     * <p>
+     * 获取分页对象
+     * </p>
+     *
+     * @return
+     */
+    protected <T> Page<T> getPage() {
+        int start = 0;
+        int length = 10;
+        if (request.getParameter(Const.LENGTH) != null) {
+            start = Integer.parseInt(request.getParameter(Const.START));
+        }
+        if (request.getParameter(Const.LENGTH) != null) {
+            length = Integer.parseInt(request.getParameter(Const.LENGTH));
+        }
+        return new Page<>(start / length + 1, length);
+    }
+
+    protected <T> EntityWrapper<T> getEntityWrapper() {
+        EntityWrapper<T> ew = new EntityWrapper<>();
+        ew.where("del_flag={0}", Const.DEL_FLAG_NORMAL);
+        return ew;
+    }
+
+    /**
+     * @param binder
+     * @Title: initBinder
+     * @Description:日期格式转换
+     * @date Apr 17, 2015
+     */
+    @InitBinder
+    public void initBinder(WebDataBinder binder) {
+        DateFormat format = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        binder.registerCustomEditor(Date.class, new CustomDateEditor(format, true));
+    }
+
+}
