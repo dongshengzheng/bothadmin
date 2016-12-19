d3481d0240c2f94373fefc79b73dfc5265d6ad64
szy <szy@szydeMacBook-Pro.local>
2016-12-19 21:27:32 +0800

    remove goodsinfo

---
+++ site/src/main/java/com/fish/idle/site/security/MyShiroRealm.java
@@ -0,0 +1,12 @@
+package com.fish.idle.site.security;
+
+import org.apache.shiro.realm.AuthorizingRealm;
+
+/**
+ * Created by szy on 19/12/2016.
+ */
+//public class MyShiroRealm extends AuthorizingRealm {
+public class MyShiroRealm {
+
+
+}
