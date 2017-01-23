6edb141a87c09c196bd1ab9e89112348e481c800
wangguocheng@shboth.cn <wangguocheng@shboth.cn>
2017-01-23 10:19:54 +0800

    send massage

--- site/src/main/java/com/fish/idle/site/web/MainConfig.java
+++ site/src/main/java/com/fish/idle/site/web/MainConfig.java
@@ -20,48 +20,48 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class MainConfig {
 
-    @Bean
-    public WxMpConfigStorage wxMpConfigStorage() {
-        WxMpInMemoryConfigStorage configStorage = new WxMpInMemoryConfigStorage();
-        configStorage.setAppId("wx91ad0b554aceca98");
-        configStorage.setSecret("9fbc68385d729ffd298bff89794a232c");
-        configStorage.setOauth2redirectUri("www.mschuangyi.com");
-        return configStorage;
-    }
-
-//    @Value("${appid}")
-//    private String appid;
-//
-//    @Value("${appsecret}")
-//    private String appsecret;
-//
-//    @Value("${token}")
-//    private String token;
-//
-//    @Value("${aeskey}")
-//    private String aesKey;
-//
-//    @Value("${partener_id}")
-//    private String partenerId;
-//
-//    @Value("${partener_key}")
-//    private String partenerKey;
-//
-//    @Value("${redirect_url}")
-//    private String url;
-//
 //    @Bean
 //    public WxMpConfigStorage wxMpConfigStorage() {
 //        WxMpInMemoryConfigStorage configStorage = new WxMpInMemoryConfigStorage();
-//        configStorage.setAppId(this.appid);
-//        configStorage.setSecret(this.appsecret);
-//        configStorage.setToken(this.token);
-//        configStorage.setAesKey(this.aesKey);
-//        configStorage.setPartnerId(this.partenerId);
-//        configStorage.setPartnerKey(this.partenerKey);
-//        configStorage.setOauth2redirectUri(this.url);
+//        configStorage.setAppId("wx91ad0b554aceca98");
+//        configStorage.setSecret("9fbc68385d729ffd298bff89794a232c");
+//        configStorage.setOauth2redirectUri("www.mschuangyi.com");
 //        return configStorage;
 //    }
+
+    @Value("${appid}")
+    private String appid;
+
+    @Value("${appsecret}")
+    private String appsecret;
+
+    @Value("${token}")
+    private String token;
+
+    @Value("${aeskey}")
+    private String aesKey;
+
+    @Value("${partener_id}")
+    private String partenerId;
+
+    @Value("${partener_key}")
+    private String partenerKey;
+
+    @Value("${redirect_url}")
+    private String url;
+
+    @Bean
+    public WxMpConfigStorage wxMpConfigStorage() {
+        WxMpInMemoryConfigStorage configStorage = new WxMpInMemoryConfigStorage();
+        configStorage.setAppId(this.appid);
+        configStorage.setSecret(this.appsecret);
+        configStorage.setToken(this.token);
+        configStorage.setAesKey(this.aesKey);
+        configStorage.setPartnerId(this.partenerId);
+        configStorage.setPartnerKey(this.partenerKey);
+        configStorage.setOauth2redirectUri(this.url);
+        return configStorage;
+    }
 
     @Bean
     public WxMpService wxMpService() {
