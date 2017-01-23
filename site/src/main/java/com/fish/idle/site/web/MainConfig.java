2e8c1ec7144fdbd1edc89c525eacd1f313729ae4
wangguocheng@shboth.cn <wangguocheng@shboth.cn>
2017-01-23 16:01:33 +0800

    send message

--- site/src/main/java/com/fish/idle/site/web/MainConfig.java
+++ site/src/main/java/com/fish/idle/site/web/MainConfig.java
@@ -23,55 +23,55 @@ import org.springframework.context.annotation.Configuration;
 public class MainConfig {
     private static final Logger LOGGER = LoggerFactory.getLogger(MainConfig.class);
     private static WxMpConfigStorage wxMpConfigStorage;
-//    @Bean
-//    public WxMpConfigStorage wxMpConfigStorage() {
-//        WxMpInMemoryConfigStorage configStorage = new WxMpInMemoryConfigStorage();
-//        configStorage.setAppId("wx91ad0b554aceca98");
-//        configStorage.setSecret("9fbc68385d729ffd298bff89794a232c");
-//        configStorage.setOauth2redirectUri("www.mschuangyi.com");
-//        return configStorage;
-//    }
-
-    @Value("${appid_site}")
-    private String appid;
-
-    @Value("${appsecret_site}")
-    private String appsecret;
-
-    @Value("${token}")
-    private String token;
-
-    @Value("${aeskey}")
-    private String aesKey;
-
-    @Value("${partener_id}")
-    private String partenerId;
-
-    @Value("${partener_key}")
-    private String partenerKey;
-
-    @Value("${redirect_url}")
-    private String url;
-
     @Bean
     public WxMpConfigStorage wxMpConfigStorage() {
-        LOGGER.error("site############################appid:"+appid);
-        LOGGER.error("site############################appsecret:"+appsecret);
-        LOGGER.error("site############################token:"+token);
-        LOGGER.error("site############################aesKey:"+aesKey);
-        LOGGER.error("site############################partenerId:"+partenerId);
-        LOGGER.error("site############################partenerKey:"+partenerKey);
-        LOGGER.error("site############################url:"+url);
         WxMpInMemoryConfigStorage configStorage = new WxMpInMemoryConfigStorage();
-        configStorage.setAppId(this.appid);
-        configStorage.setSecret(this.appsecret);
-//        configStorage.setToken(this.token);
-//        configStorage.setAesKey(this.aesKey);
-//        configStorage.setPartnerId(this.partenerId);
-//        configStorage.setPartnerKey(this.partenerKey);
-        configStorage.setOauth2redirectUri(this.url);
+        configStorage.setAppId("wx91ad0b554aceca98");
+        configStorage.setSecret("9fbc68385d729ffd298bff89794a232c");
+        configStorage.setOauth2redirectUri("www.mschuangyi.com");
         return configStorage;
     }
+
+//    @Value("${appid_site}")
+//    private String appid;
+//
+//    @Value("${appsecret_site}")
+//    private String appsecret;
+//
+//    @Value("${token}")
+//    private String token;
+//
+//    @Value("${aeskey}")
+//    private String aesKey;
+//
+//    @Value("${partener_id}")
+//    private String partenerId;
+//
+//    @Value("${partener_key}")
+//    private String partenerKey;
+//
+//    @Value("${redirect_url}")
+//    private String url;
+//
+//    @Bean
+//    public WxMpConfigStorage wxMpConfigStorage() {
+//        LOGGER.error("site############################appid:"+appid);
+//        LOGGER.error("site############################appsecret:"+appsecret);
+//        LOGGER.error("site############################token:"+token);
+//        LOGGER.error("site############################aesKey:"+aesKey);
+//        LOGGER.error("site############################partenerId:"+partenerId);
+//        LOGGER.error("site############################partenerKey:"+partenerKey);
+//        LOGGER.error("site############################url:"+url);
+//        WxMpInMemoryConfigStorage configStorage = new WxMpInMemoryConfigStorage();
+//        configStorage.setAppId(this.appid);
+//        configStorage.setSecret(this.appsecret);
+////        configStorage.setToken(this.token);
+////        configStorage.setAesKey(this.aesKey);
+////        configStorage.setPartnerId(this.partenerId);
+////        configStorage.setPartnerKey(this.partenerKey);
+//        configStorage.setOauth2redirectUri(this.url);
+//        return configStorage;
+//    }
 
     @Bean
     public WxMpService wxMpService() {
